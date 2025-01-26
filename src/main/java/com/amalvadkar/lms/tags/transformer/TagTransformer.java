ddfdbb5068853905ccb15354712f9c536f5f579d
Abhishek Malvadkar <abhi@dev.com>
2025-01-26 12:41:54 +0530

    GH-4 : Added java doc for TagTransformer method

--- src/main/java/com/amalvadkar/lms/tags/transformer/TagTransformer.java
+++ src/main/java/com/amalvadkar/lms/tags/transformer/TagTransformer.java
@@ -5,6 +5,14 @@ public interface TagTransformer {
     String ANY_NUMBER_OF_WHITESPACE_REGEX = "\\s+";
     String DASH = "-";
 
+    /**
+     * Responsible to convert user entered tag name into dashed tag name along with lowercase
+     * e.g. i/p = Spring Boot -> o/p = spring-boot
+     *
+     * @param tagName User entered tag name
+     *
+     * @return The dashed lowercase tag name
+     */
     static String transformTag(String tagName) {
         return tagName.toLowerCase().replaceAll(ANY_NUMBER_OF_WHITESPACE_REGEX, DASH);
     }
